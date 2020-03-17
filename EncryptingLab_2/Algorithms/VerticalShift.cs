a498b7271a1e68143a44887f003f412ba4160d3d
drozd0ff <botaniq.99@gmail.com>
2020-03-17 12:48:24 +0200

    Added vertical shift

---
+++ EncryptingLab_2/Algorithms/VerticalShift.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncryptingLab_2.Algorithms
+{
+    class VerticalShift : IEncrypt
+    {
+        public string MainString { get; set; }
+        private double[] key;
+        private List<double> secondList;
+        SortedDictionary<double, string> dic;
+        public VerticalShift(string main, string keyWord)
+        {
+            if (keyWord.Length >= 100)
+            {
+                throw new Exception("key word is too long, please make it less than 100 characters");
+            }
+            string[] mainStringArray = main.Split((char[]) null, StringSplitOptions.RemoveEmptyEntries);
+            MainString = String.Join("", mainStringArray);
+            dic = new SortedDictionary<double, string>();
+            key = keyWord.Select(x => (double)x).ToArray();
+        }
+
+        public void Encrypt()
+        {
+            for (int i = 0; i < secondList.Count; i++)
+            {
+                for (int j = 0; j < MainString.Length; j += secondList.Count)
+                {
+                    var item = i + j;
+                    if (item >= MainString.Length)
+                    {
+                        break;
+                    }
+
+                    if (dic.ContainsKey(secondList[i]))
+                    {
+                        dic[secondList[i]] += MainString.Substring(item, 1);
+                    }
+                    else
+                    {
+                        dic.Add(secondList[i], MainString.Substring(item, 1));
+                    }
+                }
+            }
+
+            IEnumerator<double> s = dic.Keys.GetEnumerator();
+            MainString = String.Empty;
+            while (s.MoveNext())
+            {
+                MainString += dic[s.Current] + " ";
+            }
+            s.Dispose();
+        }
+
+        public void Shuffort()
+        {
+            secondList = new List<double>();
+
+            double j = 0;
+            for (int i = 0; i < key.Length; i++, j += 0.01)
+            {
+                double item = key[i];
+                key[i] = 0;
+                if (secondList.Contains(item))
+                {
+                    item += j;
+                    secondList.Add(item);
+                }
+                else
+                {
+                    secondList.Add(item);
+                }
+            }
+        }
+
+        public void Decrypt()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Print()
+        {
+            Console.WriteLine(MainString);
+        }
+    }
+}
--- EncryptingLab_2/Program.cs
+++ EncryptingLab_2/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+using EncryptingLab_2.Algorithms;
 
 namespace EncryptingLab_2
 {
@@ -8,31 +8,39 @@ namespace EncryptingLab_2
     {
         static void Main(string[] args)
         {
+            
+            
+            
+            
+            
+            
+            //VerticalShift e1 = new VerticalShift("KOZHUKHOVSKY YAROSLAV АLEKSANDROVICH", "FOREVER");
+            //e1.Print();
+            //e1.Shuffort();
+            //e1.Encrypt();
+            //e1.Print();
 
+            //VijeneraShifr.Zavd();
 
+            //GasloviyShifr gaslo1 = new GasloviyShifr("Lulu", "slogan");
+            //gaslo1.Encrypt();
+            //gaslo1.Print();
+            //gaslo1.Decrypt();
+            //gaslo1.Print();
 
+            //LatinOneLetter latSurname = new LatinOneLetter("Drozdov");
+            //latSurname.Print();
+            //latSurname.Encrypt();
+            //latSurname.Print();
+            //latSurname.Decrypt();
+            //latSurname.Print();
 
-            VijeneraShifr.Zavd();
-
-            GasloviyShifr gaslo1 = new GasloviyShifr("Lulu", "slogan");
-            gaslo1.Encrypt();
-            gaslo1.Print();
-            gaslo1.Decrypt();
-            gaslo1.Print();
-
-            LatinOneLetter latSurname = new LatinOneLetter("Drozdov");
-            latSurname.Print();
-            latSurname.Encrypt();
-            latSurname.Print();
-            latSurname.Decrypt();
-            latSurname.Print();
-
-            CyrillicOneLetter cyrSurname = new CyrillicOneLetter("Дроздов");
-            cyrSurname.Print();
-            cyrSurname.Encrypt();
-            cyrSurname.Print();
-            cyrSurname.Decrypt();
-            cyrSurname.Print();
+            //CyrillicOneLetter cyrSurname = new CyrillicOneLetter("Дроздов");
+            //cyrSurname.Print();
+            //cyrSurname.Encrypt();
+            //cyrSurname.Print();
+            //cyrSurname.Decrypt();
+            //cyrSurname.Print();
 
             Console.ReadKey();
         }
